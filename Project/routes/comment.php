9a11248b37ec125eef3801e772b4048315080416
Vanshikaaaaaa007 <23bcs80009@cuchd.in>
2025-01-15 09:29:18 -0800

    Create comment.php

---
+++ Project/routes/comment.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav_loged.php"); ?>
+<?php 
+	if(empty($_SESSION['LoginSession'])){
+		header("location:../views/login.php");
+	}
+?>
+
+<div class="container">
+    <div class="user-comment">
+        <div class="title">
+            Post a Comment
+        </div>
+        <?php 
+            if(isset($_POST['post_comment'])){
+                $result = add_comment_to_tbl($_POST['user_comment'], $_POST['email_comment'], $_POST['roll_comment'], $_POST['comment_msg_add']);
+                echo $result;
+            }
+        ?>
+        <div class="body">
+            <?php user_commnet_add(); ?>          
+        </div>
+    </div>
+</div>
+
+<?php include("../layouts/footer.php"); ?>
