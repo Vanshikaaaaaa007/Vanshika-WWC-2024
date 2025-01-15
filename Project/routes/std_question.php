c1b727649e20a3c153e6dd3193fd1987af795d7a
Vanshikaaaaaa007 <23bcs80009@cuchd.in>
2025-01-15 09:30:31 -0800

    Create std_question.php

---
+++ Project/routes/std_question.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav_loged.php"); ?>
+
+<?php 
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
+    }
+?>
+
+<div class="container">
+    <div class="all-td-question">
+        <?php std_question_back_btn(); ?>
+
+        <div class="title">Pending Questions</div>
+        <table class="all-question-std-tbl">
+            <thead>
+                <tr>
+                    <th>Queston From</th>
+                    <th>Ask Date</th>
+                    <th>Question</th>
+                    <th>Reply By</th>
+                    <th>Reply Date</th>
+                    <th>View Question</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php all_std_question(); ?>
+            </tbody>
+        </table>
+        <hr>
+        <div class="title">Answered Questions</div>
+        <table class="all-question-std-tbl">
+            <thead>
+                <tr>
+                    <th>Queston From</th>
+                    <th>Ask Date</th>
+                    <th>Question</th>
+                    <th>Reply By</th>
+                    <th>Reply Date</th>
+                    <th>Answer</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php answer_for_question(); ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include("../layouts/footer.php"); ?>
