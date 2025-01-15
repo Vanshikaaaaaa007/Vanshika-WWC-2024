e4bb3938c1d01702bf1cacdea8b5770952660667
Vanshikaaaaaa007 <23bcs80009@cuchd.in>
2025-01-15 09:24:00 -0800

    Create footer.php

---
+++ Project/layout/footer.php
@@ -0,0 +1,6 @@
+<?php include("header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+<div class="footer">
+    <hr>
+    Copyright <i class="far fa-copyright"></i> <b>Online Quiz System</b> : 2022 All Right Reserved by <a href="https://github.com/JehanKandy" target="_blank">Jehankandy</a>
+</div>
